c8a3a0c3fa4b7d73c2b49805b6bed67c6c741c04
aigerimkudassova <aigerimkudassova@gmail.com>
2016-09-09 22:51:45 +0600

    Add files via upload

---
+++ lab13c.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespase std;
+int main()
+{
+cout <<"1	Maratov	        Maksat       	Maratovich"<<endl;
+cout <<"2	Akshabayev	Askar		Kurmanalievich"<<endl;
+cout <<"3	Kulmanov	Maksat   	Mahatovich"<<endl;
+return 0;
+}
