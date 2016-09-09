c91cafcad2b9ef4746f29ff50d851d6f51325ccc
aigerimkudassova <aigerimkudassova@gmail.com>
2016-09-09 20:02:16 +0600

    Add files via upload

---
+++ lab1.3.cpp
@@ -0,0 +1,17 @@
+#iclude <iostream>
+using namespase stt;
+
+int main()
+{
+cout <<"
+        *********         ***             *             *"endl;
+cout <<"*       *       *     *          ***           * *"endl;
+cout <<"*       *      *       *        *****         *   *"endl;
+cout <<"*       *      *       *          *          *     *"endl;
+cout <<"*       *      *       *          *         *       *"endl;
+cout <<"*       *      *       *          *          *     *"endl;
+cout <<"*       *      *       *          *           *   *"endl;
+cout <<"*       *       *     *           *            * *"endl;
+cout <<"*********         ***             *             *"endl;
+return 0;
+}
