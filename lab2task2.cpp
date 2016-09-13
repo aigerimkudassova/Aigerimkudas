220810393de8524740f1c6ad8338f70d01fcef01
aigerimkudassova <aigerimkudassova@gmail.com>
2016-09-13 21:57:14 +0600

    Add files via upload

---
+++ lab2task2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+cout<<'Enter the magnitudes"<<endl;
+double R,k,h,l,p,V,s;
+cin>>r>>k>>h;
+p=3.14;
+V=((3.14*h)/3)*(R*R+k*k);
+cout<<"V= "<<V<<endl;
+l=sqrt(h*h+(r-k)*(R-k));
+s=3.14*1*(R+k)+3.14*(R*R+k*k);
+cout<<"s= "<<s<<endl;
+system("pause>nul");
+return 0;
+}
